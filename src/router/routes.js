4fc2ef996f75a3c0afcbcbc6e0e470bc2d2f3e86
Justin <anomie7777@gmail.com>
2018-11-08 16:18:07 +0900

    [Modify] beforeEnter 함수 분리, /search 부분에도 적용

--- src/router/routes.js
+++ src/router/routes.js
@@ -45,6 +45,22 @@ function validateRefreshToken(refreshTkn, next){
   })
 }
 
+const requireAuth = (to, from, next) => {
+    console.log('start validate access token!');
+    let access = SessionStorage.get.item('accessToken');
+    let refresh = LocalStorage.get.item('refreshToken');
+
+    if(access == null){
+      //refresh token이 없으면 로그인 페이지로 리다이렉트
+      refresh == null ? next('/login') : validateRefreshToken(refresh, next);
+    }else{
+      //access token이 있으면 유효성 검사 후 메인으로
+      //access token이 무효하다면 스토리지에서 삭제 후 리프레쉬 토큰으로 액세스 토큰 발급 후 메인으로
+      validateAccessTkn(access, next);
+    }
+    console.log('finished');
+  }
+
 const routes = [
   {
     path: "/",
@@ -53,25 +69,12 @@ const routes = [
       { path: "", component: () => import("pages/Index.vue") },
       { path: "user", component: () => import("pages/account.vue") }
     ],
-    beforeEnter: (to, from, next) => {
-      console.log('start validate access token!');
-      let access = SessionStorage.get.item('accessToken');
-      let refresh = LocalStorage.get.item('refreshToken');
-
-      if(access == null){
-        //refresh token이 없으면 로그인 페이지로 리다이렉트
-        refresh == null ? next('/login') : validateRefreshToken(refresh, next);
-      }else{
-        //access token이 있으면 유효성 검사 후 메인으로
-        //access token이 무효하다면 스토리지에서 삭제 후 리프레쉬 토큰으로 액세스 토큰 발급 후 메인으로
-        validateAccessTkn(access, next);
-      }
-      console.log('finished');
-    }
+    beforeEnter: requireAuth
   },
   {
     path: "/search",
-    component: () => import("layouts/SearchLayout.vue")
+    component: () => import("layouts/SearchLayout.vue"),
+    beforeEnter: requireAuth
   },
   {
     path: "/login",
