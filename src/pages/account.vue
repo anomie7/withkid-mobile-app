447b1318333db5d8cf770ebacb3bcb23683bc315
Justin <anomie7777@gmail.com>
2018-11-16 20:31:40 +0900

    [Impl] 로그아웃 로직

--- src/pages/account.vue
+++ src/pages/account.vue
@@ -2,12 +2,23 @@
   <q-page padding>
     <!-- content -->
     사용자 페이지
+    <div @click="logout">로그아웃</div>
   </q-page>
 </template>
 
 <script>
+import { LocalStorage, SessionStorage } from 'quasar'
+
 export default {
   name: 'PageUser',
+  methods: {
+    logout(){
+      SessionStorage.remove('accessToken');
+      LocalStorage.remove('refreshToken');
+      document.location.reload();
+      alert('로그아웃 되었습니다.')
+    }
+  }
 }
 </script>
 
