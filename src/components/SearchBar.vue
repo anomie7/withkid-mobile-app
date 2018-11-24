dcf54f04aba0f5b8a06631103cac799593b3bc1e
Justin <anomie7777@gmail.com>
2018-11-24 16:52:31 +0900

    [Fix] 처음 페이지 로딩시 이벤트는 SearchBar에서 불러오도록 수정, 날짜의 기본값을 이번달로 세팅

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -169,7 +169,11 @@ export default {
         param.startDate = startOfNext.format();
         param.endDate = endOfNext.format();
       }
-  }
+  },
+  created() {
+    this.getThisMonth();
+    this.searchItem();
+  },
 }
 </script>
 
