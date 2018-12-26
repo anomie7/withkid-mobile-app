891de0f021d9649d489262b2dcdc6656c4cb710e
Justin <anomie7777@gmail.com>
2018-12-26 16:33:20 +0900

    [impl] content view 화면에서 사진, 컨텐트 이름, 시작 날짜, 끝 날짜, 카테고리, 원 출처로 이동하는 화면 구성 완료.

--- src/components/contentSwiper.vue
+++ src/components/contentSwiper.vue
@@ -5,7 +5,11 @@
       <swiper-slide v-for="(item, i) in eventLog" :key="i">
         <q-card>
           <q-card-media>
-            <img :src="BASE_URL + item.imageFilePath" @click="opened = true" :style="mediaImgHeight">
+            <img
+              :src="BASE_URL + item.imageFilePath"
+              @click="cardClick(item)"
+              :style="mediaImgHeight"
+            >
           </q-card-media>
           <q-card-title class="main-q-card-title">
             {{item.name}}
@@ -16,64 +20,168 @@
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
     <q-modal v-model="opened">
-      <h4>Basic Modal</h4>
-      <q-btn color="primary" @click="opened = false" label="Close" />
+      <div class="content-view">
+        <div class="top">
+          <!-- 이미지, 이미지 위에 뱃지 형태로 카테고리,이름, 시작 날짜, 끝 날짜, 장소(링크로 네이버 주소 검색창 띄워줌) -->
+          <img :src="imgPath" alt class="image">
+          <div style="padding: 1rem;">
+            <div class="category">{{category}}</div>
+          </div>
+          <div class="description">
+            <div>{{selectedItem.name}}</div>
+            <div>{{selectedItem.startDate}} ~ {{selectedItem.endDate}}</div>
+            <div>
+              <a
+                target="_blank"
+                :href="searchResult"
+                style="text-decoration: none"
+              >{{selectedItem.location}}</a>
+            </div>
+          </div>
+        </div>
+        <!-- 여기는 대표하는 price를 보여주고, 나머지는 접히는 박스 형태로 보여줄 것 -->
+        <!-- 원래 출처로 보내주는 버튼 -->
+        <q-btn color="primary" @click="goOriginSite" label="예매처로 이동" style="width:95%; margin: 0.5rem;" ></q-btn>
+        <!-- 네이버 검색 버튼 -->
+        <div class="close-btn" @click="opened = false" >X</div>
+      </div>
     </q-modal>
   </div>
 </template>
 
 <script>
-  import {IMAGE_BASE_URL} from './../js/global-var'
+import { IMAGE_BASE_URL } from "./../js/global-var";
 
-  export default {
-    name: "ContentSwiper",
-    props: ['eventLog'],
-    data() {
-      return {
-        swiperOption: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-          slidesOffsetBefore: 10,
-          slidesOffsetAfter: 10,
-          pagination: {
-            el: "",
-            clickable: true
-          }
-        },
-        opened: false,
-        BASE_URL: IMAGE_BASE_URL,
-        mediaImgHeight: {
-          height: '10rem'
-        }
-      };
+export default {
+  name: "ContentSwiper",
+  props: ["eventLog"],
+  methods: {
+    cardClick(seletedItem) {
+      this.opened = true;
+      this.selectedItem = seletedItem;
+    },
+    goOriginSite(){
+      window.open('http://www.daum.net ', '_blank'); 
     }
-  };
+  },
+  computed: {
+    imgPath() {
+      return this.BASE_URL + this.selectedItem.imageFilePath;
+    },
+    searchResult() {
+      return "https://www.google.com/maps?q=" + this.selectedItem.location;
+    },
+    category(){
+      switch (this.selectedItem.kindOf) {
+        case "Mu":
+           return "뮤지컬";
+          break;
+        case "Pl":
+          return "연극";
+        case "Cl":
+          return "클래식/무용"
+        case "Ex":
+          return "전시/행사"
+        default:
+          break;
+      }
+    }
+  },
+  data() {
+    return {
+      selectedItem: {
+        eventId: null,
+        imageFilePath: "",
+        kindOf: "",
+        location: "",
+        name: "",
+        startDate: null,
+        endDate: null
+      },
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        slidesOffsetBefore: 10,
+        slidesOffsetAfter: 10,
+        pagination: {
+          el: "",
+          clickable: true
+        }
+      },
+      opened: false,
+      BASE_URL: IMAGE_BASE_URL,
+      mediaImgHeight: {
+        height: "10rem"
+      }
+    };
+  }
+};
 </script>
 
 <style>
-  .swiper-container-title {
-    margin: 0 10px;
-    font-size: 15px;
-    font-weight: 600;
-  }
-  
-  .q-card-container {
-    padding: 0px;
-  }
-  
-  .main-q-card-title .q-card-title {
-    height: 4rem;
-    overflow: hidden;
-    font-size: 13px;
-    line-height: 1.2rem;
-  }
-  
-  .q-card-subtitle,
-  .q-card-title-extra {
-    font-size: 12px;
-  }
-  
-  .q-card-media img {
-    cursor: pointer;
-  }
+.swiper-container-title {
+  margin: 0 10px;
+  font-size: 15px;
+  font-weight: 600;
+}
+
+.q-card-container {
+  padding: 0px;
+}
+
+.main-q-card-title .q-card-title {
+  height: 4rem;
+  overflow: hidden;
+  font-size: 13px;
+  line-height: 1.2rem;
+}
+
+.q-card-subtitle,
+.q-card-title-extra {
+  font-size: 12px;
+}
+
+.q-card-media img {
+  cursor: pointer;
+}
+
+/* content-view start */
+.content-view .top {
+  display: flex;
+  flex-direction: column;
+}
+
+.content-view div.close-btn {
+  cursor: pointer;
+  text-align: center;
+  position: absolute !important;
+  width: 2rem;
+  height: 2rem;
+  font-size: 2rem;
+  top: 0;
+  right: 0;
+  margin: 0.5rem
+}
+
+.content-view div.category {
+  position: absolute !important;
+  text-align: center;
+  background-color: blanchedalmond;
+  border-radius: 1rem;
+}
+
+.content-view .top .description {
+  display: flex;
+  flex-direction: column;
+  padding-left: 1rem;
+}
+.content-view .top .description div {
+  margin-top: 0.8rem;
+}
+.content-view .top .image {
+  height: 20rem;
+  /* margin: 1rem; */
+}
+
+/* content-view end */
 </style>
