d3a4294ac28893682c4ff7d5c7089b1d91432e35
Justin <anomie7777@gmail.com>
2019-03-06 18:35:08 +0900

    [modify] production 환경에서 ssl 인증 적용으로 https://xxxx로 요청 가도록 수정

--- src/js/global-var.js
+++ src/js/global-var.js
@@ -1,7 +1,7 @@
-//production
-const AUTH_BASE_URL = "http://withkid.tk:8090";
-const RESOURCE_BASE_URL = "http://withkid.tk:8081";
-const IMAGE_BASE_URL = "http://withkid.tk:37772";
+// production
+const AUTH_BASE_URL = "https://withkid.tk:8090";
+const RESOURCE_BASE_URL = "https://withkid.tk:8081";
+const IMAGE_BASE_URL = "https://withkid.tk:37772";
 
 // dev
 // const AUTH_BASE_URL = "http://localhost:8082";
@@ -13,4 +13,4 @@ const IMAGE_BASE_URL = "http://withkid.tk:37772";
 // const RESOURCE_BASE_URL = "http://192.168.99.100:8081";
 // const IMAGE_BASE_URL = "http://192.168.99.100:37772"
 
-export {AUTH_BASE_URL, RESOURCE_BASE_URL, IMAGE_BASE_URL}
+export { AUTH_BASE_URL, RESOURCE_BASE_URL, IMAGE_BASE_URL };
