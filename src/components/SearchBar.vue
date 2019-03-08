7600d780cfbf76aed00d956c39ee8440775d995c
Justin <anomie7777@gmail.com>
2019-03-08 16:51:43 +0900

    [renaming] search.vue -> SearchPage.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,195 +1,207 @@
 <template>
-    <q-toolbar-title>
-          <q-select v-model="searchParam.kindOf" stack-label="카테고리" radio :options="kindOfOptions" />
-          <q-select v-model="searchParam.region" stack-label="지역" radio :options="regionOptions" />
-          <q-datetime clearable v-model="searchParam.startDate" type="date" />
-          <q-datetime clearable v-model="searchParam.endDate" type="date" />
-          <swiper class="date-chip-container">
-            <swiper-slide>            
-              <q-btn rounded push text-color="primary" color="white" @click="getThisWeek" label="이번주"> </q-btn>
-              <q-btn rounded push color="red" label="이번 주말" @click="getThisWeekEnd"> </q-btn>
-              <q-btn rounded push  color="amber" label="다음주" @click="getNextWeek"> </q-btn>
-              <q-btn rounded push color="teal" label="다음 주말" @click="getNextWeekEnd"> </q-btn>
-              <q-btn rounded push text-color="" color="light" label="이번달" @click="getThisMonth"> </q-btn>
-              <q-btn rounded push text-color="primary" color="lime" label="다음달" @click="getNextMonth"> </q-btn>
-            </swiper-slide>
-            <swiper-slide>
-              
-            </swiper-slide>
-          </swiper>
-          <q-btn icon="search" align="right" @click="searchItem"></q-btn>
-      </q-toolbar-title>
+  <q-toolbar-title>
+    <q-select v-model="searchParam.kindOf" stack-label="카테고리" radio :options="kindOfOptions"/>
+    <q-select v-model="searchParam.region" stack-label="지역" radio :options="regionOptions"/>
+    <q-datetime clearable v-model="searchParam.startDate" type="date"/>
+    <q-datetime clearable v-model="searchParam.endDate" type="date"/>
+    <swiper class="date-chip-container">
+      <swiper-slide>
+        <q-btn rounded push text-color="primary" color="white" @click="getThisWeek" label="이번주"></q-btn>
+        <q-btn rounded push color="red" label="이번 주말" @click="getThisWeekEnd"></q-btn>
+        <q-btn rounded push color="amber" label="다음주" @click="getNextWeek"></q-btn>
+        <q-btn rounded push color="teal" label="다음 주말" @click="getNextWeekEnd"></q-btn>
+        <q-btn rounded push text-color color="light" label="이번달" @click="getThisMonth"></q-btn>
+        <q-btn rounded push text-color="primary" color="lime" label="다음달" @click="getNextMonth"></q-btn>
+      </swiper-slide>
+      <swiper-slide></swiper-slide>
+    </swiper>
+    <q-btn icon="search" align="right" @click="searchItem"></q-btn>
+  </q-toolbar-title>
 </template>
 
 <script>
 import axios from "axios";
 import moment from "moment";
-import searchVue from '../pages/search.vue';
-import {RESOURCE_BASE_URL} from './../js/global-var'
+import { RESOURCE_BASE_URL } from "./../js/global-var";
 
 const BASE_URL = RESOURCE_BASE_URL;
 export default {
   // name: 'ComponentName',
-  data () {
+  data() {
     return {
       searchParam: {
-          region: null,
-          kindOf: null,
-          startDate: null,
-          endDate: null,
-          page: 0,
-          size: 10
+        region: null,
+        kindOf: null,
+        startDate: null,
+        endDate: null,
+        page: 0,
+        size: 10
       },
-      kindOfOptions: [{
-            label: '전체',
-            value: null
-          },
-          {
-            label: '뮤지컬',
-            value: 'Mu'
-          },
-          {
-            label: '연극',
-            value: 'Pl'
-          },
-          {
-            label: '클래식/무용',
-            value: 'Cl'
-          },
-          {
-            label: '전시/행사',
-            value: 'Ex'
-          }
-        ],
-        regionOptions: [{
-            label: '전체',
-            value: null
-          },
-          {
-            label: '서울',
-            value: '서울'
-          },
-          {
-            label: '부산',
-            value: '부산'
-          },
-          {
-            label: '대구',
-            value: '대구'
-          },
-          {
-            label: '인천',
-            value: '인천'
-          },
-          {
-            label: '대전',
-            value: '대전'
-          },
-          {
-            label: '광주',
-            value: '광주'
-          },
-          {
-            label: '울산',
-            value: '울산'
-          },
-          {
-            label: '세종',
-            value: '세종'
-          }
-        ]
-    }
+      kindOfOptions: [
+        {
+          label: "전체",
+          value: null
+        },
+        {
+          label: "뮤지컬",
+          value: "Mu"
+        },
+        {
+          label: "연극",
+          value: "Pl"
+        },
+        {
+          label: "클래식/무용",
+          value: "Cl"
+        },
+        {
+          label: "전시/행사",
+          value: "Ex"
+        }
+      ],
+      regionOptions: [
+        {
+          label: "전체",
+          value: null
+        },
+        {
+          label: "서울",
+          value: "서울"
+        },
+        {
+          label: "부산",
+          value: "부산"
+        },
+        {
+          label: "대구",
+          value: "대구"
+        },
+        {
+          label: "인천",
+          value: "인천"
+        },
+        {
+          label: "대전",
+          value: "대전"
+        },
+        {
+          label: "광주",
+          value: "광주"
+        },
+        {
+          label: "울산",
+          value: "울산"
+        },
+        {
+          label: "세종",
+          value: "세종"
+        }
+      ]
+    };
   },
   methods: {
-     searchItem() {
-        console.log("send request to server");
-        let $this = this;
-        let $searchParam =  Object.assign({}, this.searchParam);
-        axios
-          .get("/event", {
-            params: $searchParam,
-            baseURL: BASE_URL
-          })
-          .then(function(res) {
-            console.log('receive 500 code')
-            let returnVal = {
-              events: res.data.events,
-              searchParam: $searchParam,
-              hasNextPage: res.data.hasNextPage
-            }
-            $this.$emit('searchBarClick', returnVal);
-          })
-          .catch(function(error) {
-            if(error.response.status == 404){
-              // console.log(error.response.data.msg);
-              $this.$emit('eventNotFound', error.response.data);
-              return;
-            }
-            console.log(error);
-          });
-      },
-      containerPull(event){
-        console.log(event);
-      },
-      getThisWeek(){
-        let param = this.searchParam;
-        param.startDate = moment().format();
-        param.endDate = moment().endOf('isoWeek').format();
-        console.log(`${param.startDate} ~ ${param.endDate }`)
-      },
-      getThisWeekEnd(){
-        let param = this.searchParam;
-        let endOf = moment().endOf('isoWeek');
-        param.startDate = endOf.clone().subtract(1, 'days').format();
-        param.endDate = endOf.format();
-        console.log(`${param.startDate} ~ ${param.endDate }`);
-      },
-      getNextWeek(){
-        let param = this.searchParam;
-        let startOfNext = moment().startOf('isoWeek').add(7, 'days');
-        let endOfNext = startOfNext.clone().endOf('isoWeek');
-        console.log(`${startOfNext} ~ ${endOfNext}`)
-        param.startDate = startOfNext.format();
-        param.endDate = endOfNext.format();
-      },
-      getNextWeekEnd(){
-        let param = this.searchParam;
-        let endOfNext = moment().endOf('isoWeek').add(7, 'days');
-        let startOfNext = endOfNext.clone().subtract(1, 'days');
-        console.log(`${startOfNext} ~ ${endOfNext}`)
-        param.startDate = startOfNext.format();
-        param.endDate = endOfNext.format();
-      },
-      getThisMonth(){
-        let param = this.searchParam;
-        param.startDate = moment().format();
-        param.endDate = moment().endOf('month').format();
-        console.log(`${param.startDate} ~ ${param.endDate }`)
-      },
-      getNextMonth(){
-        let param = this.searchParam;
-        let startOfNext = moment().startOf('month').add(1, 'M');
-        let endOfNext = startOfNext.clone().endOf('month');
-        console.log(`${startOfNext} ~ ${endOfNext}`)
-        param.startDate = startOfNext.format();
-        param.endDate = endOfNext.format();
-      }
+    searchItem() {
+      console.log("send request to server");
+      let $this = this;
+      let $searchParam = Object.assign({}, this.searchParam);
+      axios
+        .get("/event", {
+          params: $searchParam,
+          baseURL: BASE_URL
+        })
+        .then(function(res) {
+          console.log("receive 500 code");
+          let returnVal = {
+            events: res.data.events,
+            searchParam: $searchParam,
+            hasNextPage: res.data.hasNextPage
+          };
+          $this.$emit("searchBarClick", returnVal);
+        })
+        .catch(function(error) {
+          if (error.response.status == 404) {
+            // console.log(error.response.data.msg);
+            $this.$emit("eventNotFound", error.response.data);
+            return;
+          }
+          console.log(error);
+        });
+    },
+    containerPull(event) {
+      console.log(event);
+    },
+    getThisWeek() {
+      let param = this.searchParam;
+      param.startDate = moment().format();
+      param.endDate = moment()
+        .endOf("isoWeek")
+        .format();
+      console.log(`${param.startDate} ~ ${param.endDate}`);
+    },
+    getThisWeekEnd() {
+      let param = this.searchParam;
+      let endOf = moment().endOf("isoWeek");
+      param.startDate = endOf
+        .clone()
+        .subtract(1, "days")
+        .format();
+      param.endDate = endOf.format();
+      console.log(`${param.startDate} ~ ${param.endDate}`);
+    },
+    getNextWeek() {
+      let param = this.searchParam;
+      let startOfNext = moment()
+        .startOf("isoWeek")
+        .add(7, "days");
+      let endOfNext = startOfNext.clone().endOf("isoWeek");
+      console.log(`${startOfNext} ~ ${endOfNext}`);
+      param.startDate = startOfNext.format();
+      param.endDate = endOfNext.format();
+    },
+    getNextWeekEnd() {
+      let param = this.searchParam;
+      let endOfNext = moment()
+        .endOf("isoWeek")
+        .add(7, "days");
+      let startOfNext = endOfNext.clone().subtract(1, "days");
+      console.log(`${startOfNext} ~ ${endOfNext}`);
+      param.startDate = startOfNext.format();
+      param.endDate = endOfNext.format();
+    },
+    getThisMonth() {
+      let param = this.searchParam;
+      param.startDate = moment().format();
+      param.endDate = moment()
+        .endOf("month")
+        .format();
+      console.log(`${param.startDate} ~ ${param.endDate}`);
+    },
+    getNextMonth() {
+      let param = this.searchParam;
+      let startOfNext = moment()
+        .startOf("month")
+        .add(1, "M");
+      let endOfNext = startOfNext.clone().endOf("month");
+      console.log(`${startOfNext} ~ ${endOfNext}`);
+      param.startDate = startOfNext.format();
+      param.endDate = endOfNext.format();
+    }
   },
   created() {
     this.getThisMonth();
     this.searchItem();
-  },
-}
+  }
+};
 </script>
 
 <style>
-.date-chip-container{
+.date-chip-container {
   margin: 0.5em auto;
 }
-.touch-to-left{
+.touch-to-left {
   transform: translateX(-167px);
 }
-.swiper-slide{
+.swiper-slide {
   background-color: initial !important;
 }
 </style>
--- src/layouts/SearchLayout.vue
+++ src/layouts/SearchLayout.vue
@@ -5,110 +5,121 @@
         <search-bar @searchBarClick="receiveSearchbarRes" @eventNotFound="eventNotFound"></search-bar>
       </q-toolbar>
     </q-layout-header>
-  
+
     <q-page-container class="scroll">
       <search-component :events="events" :hasNextPage="hasNextPage"></search-component>
     </q-page-container>
-  
+
     <q-layout-footer>
       <q-toolbar color="amber">
-        <q-tabs class="bottoms-qtaps" color="amber" position="bottom" text-color="dark" align="justify">
-          <q-route-tab slot="title" to="/" icon="home" />
-          <q-route-tab default slot="title" to="/search" icon="search" />
-          <q-route-tab slot="title" to="/user" icon="account_box" />
+        <q-tabs
+          class="bottoms-qtaps"
+          color="amber"
+          position="bottom"
+          text-color="dark"
+          align="justify"
+        >
+          <q-route-tab slot="title" to="/" icon="home"/>
+          <q-route-tab default slot="title" to="/search" icon="search"/>
+          <q-route-tab slot="title" to="/user" icon="account_box"/>
         </q-tabs>
       </q-toolbar>
     </q-layout-footer>
-    <q-btn v-back-to-top.animate="{offset: 100, duration: 0}" round color="primary" class="fixed-bottom-right animate-pop" style="margin: 0 15px 65px 0;z-index:3">
-      <q-icon name="keyboard_arrow_up" />
+    <q-btn
+      v-back-to-top.animate="{offset: 100, duration: 0}"
+      round
+      color="primary"
+      class="fixed-bottom-right animate-pop"
+      style="margin: 0 15px 65px 0;z-index:3"
+    >
+      <q-icon name="keyboard_arrow_up"/>
     </q-btn>
   </q-layout>
 </template>
 
 <script>
-  import {openURL} from "quasar";
-  import { debounce} from "quasar";
-  import axios from "axios";
-  import SearchComponent from "../pages/search";
-  import SearchBar from '../components/SearchBar.vue'
-  import {RESOURCE_BASE_URL} from './../js/global-var'
+import { openURL } from "quasar";
+import { debounce } from "quasar";
+import axios from "axios";
+import SearchComponent from "../pages/SearchPage";
+import SearchBar from "../components/SearchBar.vue";
+import { RESOURCE_BASE_URL } from "./../js/global-var";
 
-  const BASE_URL = RESOURCE_BASE_URL;
-  export default {
-    name: "SearchLayout",
-    data() {
-      return {
-        events: [],
-        currentPageNum: 0,
-        hasNextPage: false,
-        searchParam: {
-          region: null,
-          kindOf: null,
-          startDate: null,
-          endDate: null,
-          page: 0,
-          size: 10
+const BASE_URL = RESOURCE_BASE_URL;
+export default {
+  name: "SearchLayout",
+  data() {
+    return {
+      events: [],
+      currentPageNum: 0,
+      hasNextPage: false,
+      searchParam: {
+        region: null,
+        kindOf: null,
+        startDate: null,
+        endDate: null,
+        page: 0,
+        size: 10
+      }
+    };
+  },
+  methods: {
+    openURL,
+    scrolled(scroll) {
+      let a = window.innerHeight + scroll.position + 1;
+      let b = document.body.offsetHeight;
+      if (a >= b) {
+        if (this.hasNextPage) {
+          this.searchParam.page++;
+          this.search();
+        } else if (!this.hasNextPage) {
+          console.log("no more events obj!");
         }
-      };
-    },
-    methods: {
-      openURL,
-      scrolled(scroll) {
-        let a = window.innerHeight + scroll.position + 1;
-        let b = document.body.offsetHeight;
-        if (a >= b) {
-          if (this.hasNextPage) {
-            this.searchParam.page++;
-            this.search();
-          }else if(!this.hasNextPage){
-            console.log('no more events obj!')
-          }
-          console.log(`${a}  ${b}`);
-        }
-      },
-      search() {
-        console.log("send request to server");
-        let $this = this;
-        axios
-          .get("/event", {
-            params: $this.searchParam,
-             baseURL: BASE_URL
-          })
-          .then(function(res) {
-            let $event = res.data;
-            console.log($event);
-            $this.hasNextPage = $event.hasNextPage;
-            $this.currentPageNum = $event.currentPageNum;
-            $this.events = $this.events.concat($event.events);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      },
-      receiveSearchbarRes($data){
-        console.log('handling searchBarClick event')
-        console.log($data);
-        this.hasNextPage = $data.hasNextPage;
-        this.events = $data.events;
-        this.searchParam = $data.searchParam;
-      },
-      eventNotFound(response){
-        console.log(response.msg);
-        this.events = [];
-        this.hasNextPage = false;
+        console.log(`${a}  ${b}`);
       }
     },
-    created() {
+    search() {
+      console.log("send request to server");
+      let $this = this;
+      axios
+        .get("/event", {
+          params: $this.searchParam,
+          baseURL: BASE_URL
+        })
+        .then(function(res) {
+          let $event = res.data;
+          console.log($event);
+          $this.hasNextPage = $event.hasNextPage;
+          $this.currentPageNum = $event.currentPageNum;
+          $this.events = $this.events.concat($event.events);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
-    components: {
-      SearchComponent,
-      SearchBar
+    receiveSearchbarRes($data) {
+      console.log("handling searchBarClick event");
+      console.log($data);
+      this.hasNextPage = $data.hasNextPage;
+      this.events = $data.events;
+      this.searchParam = $data.searchParam;
+    },
+    eventNotFound(response) {
+      console.log(response.msg);
+      this.events = [];
+      this.hasNextPage = false;
     }
-  };
+  },
+  created() {},
+  components: {
+    SearchComponent,
+    SearchBar
+  }
+};
 </script>
 
 <style>
-  .bottoms-qtaps {
-    width: 100%;
-  }
+.bottoms-qtaps {
+  width: 100%;
+}
 </style>
