07d817b66f160674eee5b8e7650a0a66a07b02b9
Justin <anomie7777@gmail.com>
2019-03-08 16:52:09 +0900

    [renaming[ index.vue -> MainPage.vue

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-page class="scroll">
+    <q-carousel class="text-white" style="height:150px;margin-bottom:10px">
+      <q-carousel-slide class="bg-primary">광고 기획전</q-carousel-slide>
+      <q-carousel-slide class="bg-secondary">기획전 2</q-carousel-slide>
+      <q-carousel-slide class="bg-tertiary">몰라 3</q-carousel-slide>
+    </q-carousel>
+    <content-swiper v-for="k in [1]" :event-log="latestClickLogs" :key="k"></content-swiper>
+    <div style="padding: 10px;">
+      <div class="main-footer">
+        <ul style="padding: 0 10px;margin: 0;">footer
+          <li>대표: test</li>
+          <li>정보책임자: test</li>
+          <li>본 서비스는 정보 제공을 목적으로 하며 각 컨텐츠에 대한 책임은 주최사와 판매업체에 있음을 고시함</li>
+        </ul>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+.swiper-slide {
+  background-color: white;
+}
+
+.swiper-container {
+  margin-bottom: 5px;
+  padding: 5px 0;
+}
+
+.main-footer {
+  font-size: 0.5em;
+  width: 100%;
+  padding: 5px;
+  border: 1px solid black;
+}
+</style>
+
+<script>
+import contentSwiper from "./../components/ContentSwiper";
+import { RESOURCE_BASE_URL } from "./../js/global-var";
+import { SessionStorage } from "quasar";
+import axios from "axios";
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      latestClickLogs: null
+    };
+  },
+  methods: {
+    async getLatestEvents() {
+      const BASE_URL = RESOURCE_BASE_URL;
+      let accessTkn = SessionStorage.get.item("accessToken");
+      try {
+        let result = await axios.get("/eventLog", {
+          headers: {
+            Authorization: accessTkn
+          },
+          baseURL: BASE_URL
+        });
+        this.latestClickLogs = result.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+  components: {
+    contentSwiper
+  },
+  created() {
+    this.getLatestEvents();
+  }
+};
+</script>
