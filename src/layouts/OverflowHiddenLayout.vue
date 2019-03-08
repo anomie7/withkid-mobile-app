7de84ba3a6b1a4bd197505e49af1bbcd821aa7b2
Justin <anomie7777@gmail.com>
2019-03-08 16:42:21 +0900

    [impl] 로그인 화면과 회원가입 화면을 감싸줄 레이아웃 파일 추가. 이전에 body에 overflow:hidden 설정 해주면서 search나 index 화면에서도 overflow:hidden이 유지되면서 스크롤 안내려가던 문제 수정

---
+++ src/layouts/OverflowHiddenLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="aliceblue-wrapper">
+    <q-layout class="overflow-hidden-layout">
+      <div class="join-login-container">
+        <router-view/>
+      </div>
+    </q-layout>
+  </div>
+</template>
+<script>
+import { openURL } from "quasar";
+
+export default {
+  name: "OverflowHiddenLayout",
+  data() {
+    return {};
+  },
+  methods: {
+    openURL
+  }
+};
+</script>
+<style>
+#aliceblue-wrapper {
+  background-color: aliceblue;
+  position: absolute;
+  width: 100%;
+}
+
+#aliceblue-wrapper .overflow-hidden-layout {
+  overflow: hidden;
+}
+#aliceblue-wrapper .overflow-hidden-layout .join-login-container {
+  margin: 0 auto;
+  width: 85%;
+}
+</style>
