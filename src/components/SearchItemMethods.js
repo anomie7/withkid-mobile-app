b699a71098cf1f7b03a38205b7911cee5c51d1b1
Justin <anomie7777@gmail.com>
2019-03-29 18:31:32 +0900

    [modify] eventLog를 저장하던 서버를 resource server에서 ulog-server로 변경 uri만 변경

--- src/components/SearchItemMethods.js
+++ src/components/SearchItemMethods.js
@@ -1,56 +1,58 @@
-import axios from 'axios';
-import { LocalStorage, SessionStorage } from 'quasar'
-import {AUTH_BASE_URL, RESOURCE_BASE_URL} from './../js/global-var'
+import axios from "axios";
+import { LocalStorage, SessionStorage } from "quasar";
+import { AUTH_BASE_URL, USERLOG_BASE_URL } from "./../js/global-var";
 
 //eventLog 기록할 때 엑세스 토큰이 무효한 경우 리프레쉬 토큰으로 액세스 토큰 발급받는 로직
-function validateRefreshToken(refreshTkn, $router, $item){
-    axios({
-      method: 'post',
-      url: '/accessToken',
-      headers: {
-        'Authorization': refreshTkn
-      },
-      baseURL: AUTH_BASE_URL
-    }).then(function(res){
+function validateRefreshToken(refreshTkn, $router, $item) {
+  axios({
+    method: "post",
+    url: "/accessToken",
+    headers: {
+      Authorization: refreshTkn
+    },
+    baseURL: AUTH_BASE_URL
+  })
+    .then(function(res) {
       //refresh token으로 aceess token 발급
       console.log(res);
-      if(res.status == 201){
+      if (res.status == 201) {
         LocalStorage.set("refreshToken", res.data.refreshToken);
       }
       SessionStorage.set("accessToken", res.data.accessToken);
       storeEventLog($item, $router);
     })
-    .catch(function(err){
+    .catch(function(err) {
       console.log(err.response.data.msg);
       //refresh token이 무효하다면(서버에서 exception 발생) 스토리지에서 삭제 후 로그인 페이지로
-      LocalStorage.remove('refreshToken');
-      $router.replace('/login');
-    })
-  }
+      LocalStorage.remove("refreshToken");
+      $router.replace("/login");
+    });
+}
 
-  function storeEventLog($item, $router){
-    let accessTkn = SessionStorage.get.item('accessToken');
-     axios({
-      method: 'post',
-      url: '/eventLog',
-      headers: {
-        'Authorization': accessTkn
-      },
-      data: {
-         ...$item
-      },
-      baseURL: RESOURCE_BASE_URL
-    }).then(function(res){
-      console.log(res.data)
+function storeEventLog($item, $router) {
+  let accessTkn = SessionStorage.get.item("accessToken");
+  axios({
+    method: "post",
+    url: "/eventLog",
+    headers: {
+      Authorization: accessTkn
+    },
+    data: {
+      ...$item
+    },
+    baseURL: USERLOG_BASE_URL
+  })
+    .then(function(res) {
+      console.log(res.data);
     })
-    .catch(function(err){
-      console.log(err.response.data)
-      if(err.response.status == 401){
-        let refresh = LocalStorage.get.item('refreshToken')
-        console.log('401 error')
+    .catch(function(err) {
+      console.log(err.response.data);
+      if (err.response.status == 401) {
+        let refresh = LocalStorage.get.item("refreshToken");
+        console.log("401 error");
         validateRefreshToken(refresh, $router, $item);
       }
-    })
-  }
+    });
+}
 
-  export {validateRefreshToken, storeEventLog};
+export { validateRefreshToken, storeEventLog };
